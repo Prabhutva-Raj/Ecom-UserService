d40bdb07a02cbdb9ac19c1966ba8cd0801ccdfaf
P R A B H U <prabhu@MacBook-Pro.local>
2025-08-10 12:30:11 +0530

    added AuthController, wrote AuthService.signup() method, added DTOs

--- src/main/java/dev/prabhu/UserService/controller/AuthController.java
+++ src/main/java/dev/prabhu/UserService/controller/AuthController.java
@@ -1,4 +1,56 @@
 package dev.prabhu.UserService.controller;
 
+import dev.prabhu.UserService.dto.*;
+import dev.prabhu.UserService.model.Session;
+import dev.prabhu.UserService.model.SessionStatus;
+import dev.prabhu.UserService.model.User;
+import dev.prabhu.UserService.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDto> login(@RequestBody LoginRequestDto request) {
+        return authService.login(request.getEmail(), request.getPassword());
+    }
+
+    @PostMapping("/logout/{id}")
+    public ResponseEntity<Void> logout(@PathVariable("id") Long userId, @RequestHeader("token") String token) {
+        return authService.logout(token, userId);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserDto> signUp(@RequestBody SignUpRequestDto request) {
+        UserDto userDto = authService.signup(request.getEmail(), request.getPassword());
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
+
+    @PostMapping("/validate")
+    public ResponseEntity<SessionStatus> validateToken(@RequestBody ValidateTokenRequestDto request) {
+        SessionStatus sessionStatus = authService.validate(request.getToken(), request.getUserId());
+
+        return new ResponseEntity<>(sessionStatus, HttpStatus.OK);
+    }
+
+    //below APIs are only for learning purposes, should not be present in actual systems
+    /*@GetMapping("/session")
+    public ResponseEntity<List<Session>> getAllSession(){
+        return authService.getAllSession();
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return authService.getAllUsers();
+    }*/
 }
